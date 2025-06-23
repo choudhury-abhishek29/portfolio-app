009ea22d245c08c2a96739bda602db133993e7b7
choudhury-abhishek29 <choudhury.abhishek29@gmail.com>
2025-06-22 19:28:05 -0700

    Initial commit of React app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Portfolio goes here
         </a>
       </header>
     </div>
